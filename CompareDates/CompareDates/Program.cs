93bec52fd965014c23f3df596dfcf244273d9493
BlancaLujan <77300324+BlancaLujan@users.noreply.github.com>
2021-01-22 20:47:50 -0700

    Compare dates

---
+++ CompareDates/CompareDates/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CompareDates
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime myDateTimeVariable = new DateTime(12 / 09 / 2011);
+            DateTime myDateTimeVariable2 = DateTime.Now;
+
+            int result = DateTime.Compare(myDateTimeVariable, myDateTimeVariable2);
+
+            Console.WriteLine(result);
+        }
+    }
+    
+}
